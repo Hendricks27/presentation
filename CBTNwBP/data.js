787c11cd82d084d71c6c4f34b435437031cdb8ca
Hendricks27 <wenjinzhang95@gmail.com>
2019-09-02 18:51:47 -0400

    New data model

---
+++ CBTNwBP/data.js
@@ -0,0 +1,39 @@
+"use strict";
+
+var data = {
+    "G98716VN": {
+        children: ["G57037YM"],
+        type: "composition",
+        comp: {"GlcNAc": 1, "Man": 1, "Gal": 1}
+    },
+    "G15584LN": {
+        children: ["G09616TM"],
+        type: "composition",
+        comp: {"GlcNAc": 1, "Xxx": 1, "Gal": 1}
+    },
+    "G67438WM": {
+        children: [],
+        type: "saccharide",
+        comp: {"GlcNAc": 1, "Man": 1, "Gal": 1}
+    },
+    "G83868CR": {
+        children: [],
+        type: "saccharide",
+        comp: {"GlcNAc": 1, "Xxx": 1, "Gal": 1}
+    },
+    "G57037YM": {
+        children: ["G67438WM"],
+        type: "topology",
+        comp: {"GlcNAc": 1, "Man": 1, "Gal": 1}
+    },
+    "G09616TM": {
+        children: ["G83868CR"],
+        type: "topology",
+        comp: {"GlcNAc": 1, "Xxx": 1, "Gal": 1}
+    },
+    "G00012MO": {
+        children: [],
+        type: "saccharide",
+        comp: {"Glc": 1, "GalNAc": 1, "Gal": 2}
+    }
+};
